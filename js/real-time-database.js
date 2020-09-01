2418aaaa9a10e87a5926ef98e2626ca4fd71f47d
Messias Nunesmaia <messias.nunesmaia@ucsal.edu.br>
2020-09-01 08:12:20 -0300

    atualização

--- js/real-time-database.js
+++ js/real-time-database.js
@@ -1,35 +1,35 @@
-var ip = document.getElementById('full-name');
-var nome = document.getElementById('user-ip');
-var email = document.getElementById('email-address');
-var telefone = document.getElementById('telephone');
-var addButton = document.getElementById('enviar-formulario');
+// var ip = document.getElementById('full-name');
+// var nome = document.getElementById('user-ip');
+// var email = document.getElementById('email-address');
+// var telefone = document.getElementById('telephone');
+// var addButton = document.getElementById('enviar-formulario');
 
-const db = firebase.database();
-const rootRef = db.ref('forms_users');
+// const db = firebase.database();
+// const rootRef = db.ref('forms_users');
 
-addButton.addEventListener('click', (teste) => {
-
-    teste.preventDefault();
-
-    // const email = rootRef.push().key
-    rootRef.child(email).set({
-
-        ip: ip,
-        nome: nome.value,
-        telefone: telefone
-
-    });
-
-});
-
-// updateButton.addEventListener('click', (teste) => {
+// addButton.addEventListener('click', (teste) => {
 
 //     teste.preventDefault();
 
-//     const newData = {
+//     // const email = rootRef.push().key
+//     rootRef.child(email).set({
 
-//         age: age.value
+//         ip: ip,
+//         nome: nome.value,
+//         telefone: telefone
 
-//     };
+//     });
 
 // });
+
+// // updateButton.addEventListener('click', (teste) => {
+
+// //     teste.preventDefault();
+
+// //     const newData = {
+
+// //         age: age.value
+
+// //     };
+
+// // });
