ff632c3c971a578b7d64cd2fc87f89ba8806a04b
Messias Nunesmaia <messias.lsn@gmail.com>
2023-04-23 10:19:41 -0300

    Initial changes do minimalist style

---
+++ index.js
@@ -0,0 +1,8 @@
+// Smooth roll
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
